acd27d66f12f5cb578dcab4c6b0c43fe2c2fb204
Pavel Zakharov <pzakharov83@gmail.com>
2022-01-30 13:43:47 +0300

    Add CLI options

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 import asyncio
+import aiofiles
+import datetime as dt
+import configargparse
 
 
-async def read_message():
+async def read_message(options):
     reader, _ = await asyncio.open_connection(
-        'minechat.dvmn.org', 5000
+        options.host, options.port
     )
 
     while True:
-        data = await reader.readline()
-        print(f'{data.decode().rstrip()}')
+        async with aiofiles.open(options.history, mode='a') as f:
+            data = await reader.readline()
+            print(f'{dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S")} {data.decode().rstrip()}')
+            await f.write(f'{dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S")} {data.decode()}')
         await asyncio.sleep(0)
 
 
-async def main():
+async def main(options):
     loop = asyncio.get_event_loop()
-    read_func = loop.create_task(read_message())
+    read_func = loop.create_task(read_message(options))
     
     while True:
         await read_func
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    configs = configargparse.ArgParser(default_config_files=['.my_settings',])
+    configs.add('-s', '--host', default='minechat.dvmn.org', help='host to connect')
+    configs.add('-p', '--port', default=5000, help='port to use')
+    configs.add('-l', '--history', default='chat.log', help='file to log')
+    options = configs.parse_args()
+    try:
+        asyncio.run(main(options))
+    except KeyboardInterrupt:
+        pass
