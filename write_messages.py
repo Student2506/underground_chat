ddc94372cc7821e2f4025998f9ee8d767466dcda
Pavel Zakharov <pzakharov83@gmail.com>
2022-01-30 15:49:25 +0300

    Add check for broken token.

--- write_messages.py
+++ write_messages.py
@@ -1,9 +1,11 @@
 import asyncio
 import configargparse
 import logging
+import json
 
 
-FORMAT = '%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+# FORMAT_OLD = '%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+FORMAT = '%(levelname)s:%(funcName)s:%(message)s'
 ACCOUNT = '6c791ca6-81c4-11ec-8c47-0242ac110002'
 
 
@@ -13,13 +15,16 @@ log = logging.getLogger(__name__)
 
 async def write_message(options):
     reader, writer = await asyncio.open_connection(
-        options.host, 5050
+        options.host, options.port 
     )
     await asyncio.sleep(5)
     data = await reader.readline()
     log.debug(data.decode())
     writer.write((ACCOUNT + '\n').encode())
     data = await reader.readline()
+    if json.loads(data.decode()) is None:
+        log.debug('Неизвестный токен. Проверьте его или зарегистрируйте заново.')
+        return
     log.debug(data.decode())
     await asyncio.sleep(5)
     message = 'Я снова тестирую чатик. Это третье сообщение.\n\n'
