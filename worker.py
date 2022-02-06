1438cdee099a18b440cb1b45567b9f0dd8e9c699
Захаров Павел <pzakharov83@gmail.com>
2022-02-06 22:13:44 +0300

    added task group

--- worker.py
+++ worker.py
@@ -156,6 +156,8 @@ async def main():
         '-send', '--secondary_port', default=5050, help='port to send messages'
     )
     options = configs.parse_args()
+    print(options)
+    exit()
 
     host = options.host
     port = options.port
